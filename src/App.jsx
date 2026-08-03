2b3411a36b0dd86753f5a3e8becb8ff3d15620fa
Oceansiam <u6640031@au.edu>
2026-08-03 23:23:32 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import "./App.css";
 import Greetings from "./components/Greetings";
 import { Routes, Route} from "react-router-dom";
+import { BrowserRouter} from 'react-router-dom';
 
 function App() {
   return (
+    <BrowserRouter>
     <Routes>
       <Route path="/"about element={<Greetings message="Welcome to My World" />} />
       <Route path="/profile" element={<Greetings message="Profile Page" />}/>
     </Routes>
+    </BrowserRouter>
     // <div className="card">
     //   <Greetings message="This content is rendered on the browser" />
     // </div>
